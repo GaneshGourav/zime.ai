ec2e3f3bd2f20d354ae4d8e30bde8a919e4ea073
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-05-02 17:48:48 +0530

    project initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { TableData } from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+    <TableData/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const TableData = ()=>{
+    return <>
+    <h1>Ant table</h1>
+    
+    </>
+}
